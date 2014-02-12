c0aae9927b8912a8736b77288036110f6cab0f45
Victor Rivas <vctr90w@gmail.com>
2014-02-12 11:26:52 -0500

    Designing product page

--- js/content.js
+++ js/content.js
@@ -29,9 +29,9 @@ function pLoads(prod){
 	if(prod=='e'){
 		var url = 'contents/products/electrodes.html';
 	}else if(prod=='w'){
-		
+		var url = 'contents/products/wire.html';
 	}else if(prod=='f'){
-
+		var url = 'contents/products/fund.html';
 	}
 	$.get(
 			url,
@@ -42,4 +42,26 @@ function pLoads(prod){
 			$('.oShow').html(data);
 		}
 	);
+}
+
+function productsL(p){
+	if(p=='a63'){
+		var url = 'contents/products/proList/a63.html';
+	}else if(p=='a78'){
+		var url = 'contents/products/proList/a78.html';
+	}else if(p=='a60'){
+		var url = 'contents/products/proList/a60.html';
+	}else if(p=='azul'){
+		var url = 'cotents/products/proList/azul.html';
+	}
+	$.get(
+			url,
+		{
+			//varPage:null
+		},
+		function(data){
+			$('.pShow').html(data);
+		}
+	);
+
 }
